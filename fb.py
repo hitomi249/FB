873d4830c4eb147f1a49b59705df3fcd17b11580
hitomi249 <coloful.i.o.i@gmai.com>
2023-10-16 23:18:29 +0900

    fizzbuzz関数を作成

--- fb.py
+++ fb.py
@@ -5,7 +5,13 @@
 
 
 def fizzbuzz(i: int) -> str:
-    pass
+    if i % 3 == 0 and i % 5 == 0:
+        return "fizzbuzz"
+    if i % 3 == 0:
+        return "fizz"
+    if i % 5 == 0:
+        return "buzz"
+    return str(i)
 
 
 if __name__ == "__main__":
